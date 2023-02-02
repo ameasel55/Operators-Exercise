882d8104f539ef8a5041d0b9919a4ffe5ee20d41
Adam Measel <ameasel55@gmail.com>
2023-02-02 14:03:28 -0600

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+            int b = 4;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4);
+            Console.WriteLine("17/4 is 4 remainder 1");
         }
     }
 }
